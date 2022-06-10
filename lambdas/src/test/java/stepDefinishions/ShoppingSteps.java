9093fc05cfc6e46927224ce11a829f07dbc614ac
Petya Tsvetkova <petya.yaneva-tsvetkova@estafet.com>
2022-06-10 16:57:19 +0300

    commit

--- lambdas/src/test/java/stepDefinishions/ShoppingSteps.java
+++ lambdas/src/test/java/stepDefinishions/ShoppingSteps.java
@@ -1,4 +1,5 @@
 package stepDefinishions;
+
 import io.cucumber.java8.En;
 import org.junit.jupiter.api.Assertions;
 
@@ -6,12 +7,16 @@ public class ShoppingSteps implements En {
     private int budget = 0;
 
     public ShoppingSteps() {
-//        Given("I have {int} in my wallet", (Integer money) -> budget = money);
-//        Given("^I have (\\d+) in my wallet$", (Integer money) -> budget = money);
-        Given("I have (\\d+) in my wallet", (Integer money) -> budget = money);
-        When("I buy {word} with {int}", (String product, Integer price) -> budget -= price);
 
-        Then("I should have {int} in my wallet", (Integer finalBudget) ->
+        Given("^I have (\\d+) in my wallet$", (Integer money) -> budget = money);
+        When("^I buy .* with (\\d+)$", (Integer price) -> budget -= price);
+        Then("^I should have (\\d+) in my wallet$", (Integer finalBudget) ->
                 Assertions.assertEquals(budget, finalBudget.intValue()));
+
+        //Option 2:
+//        Given("I have {int} in my wallet", (Integer money) -> budget = money);
+//        When("I buy {word} with {int}", (String product, Integer price) -> budget -= price);
+//        Then("I should have {int} in my wallet", (Integer finalBudget) ->
+//                Assertions.assertEquals(budget, finalBudget.intValue()));
     }
 }
